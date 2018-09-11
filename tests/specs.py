de1d1f270e8f55c25af06c98ffa739c2a449e65d
Théo Guerin <theo.guerin.pro@gmail.com>
2018-09-11 15:55:12 +0200

    test overide by file

--- tests/specs.py
+++ tests/specs.py
@@ -12,9 +12,18 @@ class TestConfigEnv(unittest.TestCase):
         return os.path.dirname(os.path.abspath(__file__))+"/"
 
     def test_addFileJson(self):
-        config = ConfigEnv.ConfigEnv(self.getCurrentPath()+"data/config.json")
+        config = ConfigEnv.ConfigEnv()
+        config.addFile(self.getCurrentPath()+"data/config.json")
         self.assertEqual( config._config['DEFAULT']['SECRET_KEY'], "secret-key-of-myapp" )
 
     def test_addFileIni(self):
-        config = ConfigEnv.ConfigEnv(self.getCurrentPath()+"data/config.ini")
-        self.assertEqual( config._config['DEFAULT']['SECRET_KEY'], "secret-key-of-myapp" )
+        config = ConfigEnv.ConfigEnv()
+        config.addFile(self.getCurrentPath()+"data/config.ini")
+        self.assertEqual( config._config['TEST']['TEST_TMP_DIR'], "tests" )
+
+    def test_overideFile(self):
+        config = ConfigEnv.ConfigEnv()
+        config.addFile(self.getCurrentPath()+"data/config.json")
+        self.assertEqual( config._config['CI']['SERVICE'], "travis-ci" )
+        config.addFile(self.getCurrentPath()+"data/config.ini")
+        self.assertEqual( config._config['CI']['SERVICE'], "plus-travis-ci" )
