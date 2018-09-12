c0f274fa297c42f898b70848e4d017a15fc3dc7c
Théo Guerin <theo.guerin.pro@gmail.com>
2018-09-12 11:53:06 +0200

    réorganisation des tests

--- tests/specs.py
+++ tests/specs.py
@@ -1,35 +1,38 @@
 import os, sys
-parentPath = os.path.abspath("..")
-if parentPath not in sys.path:
-    sys.path.insert(0, parentPath)
+modulePath = os.path.abspath("../ConfigEnv")
+if modulePath not in sys.path:
+    sys.path.insert(0, modulePath)
 
 import unittest
-import ConfigEnv
+from ConfigEnv import Config
 
-class TestConfigEnv(unittest.TestCase):
+# print(ConfigEnv.__dict__.keys() )
+ # print(ConfigEnv)
+
+class TestConfig(unittest.TestCase):
 
     def getCurrentPath(self):
         return os.path.dirname(os.path.abspath(__file__))+"/"
 
     def test_addFileJson(self):
-        config = ConfigEnv.ConfigEnv()
+        config = Config()
         config.addFile(self.getCurrentPath()+"data/config.json")
         self.assertEqual( config._config['DEFAULT']['SECRET_KEY'], "secret-key-of-myapp" )
 
     def test_addFileIni(self):
-        config = ConfigEnv.ConfigEnv()
+        config = Config()
         config.addFile(self.getCurrentPath()+"data/config.ini")
         self.assertEqual( config._config['TEST']['TEST_TMP_DIR'], "tests" )
 
     def test_overideFile(self):
-        config = ConfigEnv.ConfigEnv()
+        config = Config()
         config.addFile(self.getCurrentPath()+"data/config.json")
         self.assertEqual( config._config['CI']['SERVICE'], "travis-ci" )
         config.addFile(self.getCurrentPath()+"data/config.ini")
         self.assertEqual( config._config['CI']['SERVICE'], "plus-travis-ci" )
 
     def test__recursiveRoute(self):
-        config = ConfigEnv.ConfigEnv()
+        config = Config()
         fakeContext = {
             "a" : {
                 "b_c" : "yep yap"
