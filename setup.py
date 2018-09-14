5032ffc4d98af7ff4a8d7bb6474eb328ff675477
Théo Guerin <theo.guerin.pro@gmail.com>
2018-09-14 16:44:31 +0200

    ajout build travis

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ConfigEnv',
-    version='1.1.0',
+    version='1.1.1',
     author="Théo \"Nydareld\" Guerin",
     author_email="theo.guerin.pro@gmail.com",
     description="Gestionnaire de configuration en json, ini avec overide possible en variable d’environnement",
